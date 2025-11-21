c3d69046a4f9cccc3fc3925457b49c087723d474
Parth Dandekar <parthd2d@gmail.com>
2025-11-21 14:30:18 +0530

    added local config

--- android/examples/run-local-test/local.conf.js
+++ android/examples/run-local-test/local.conf.js
@@ -1,35 +1,49 @@
 exports.config = {
-  user: process.env.BROWSERSTACK_USERNAME || 'BROWSERSTACK_USERNAME',
-  key: process.env.BROWSERSTACK_ACCESS_KEY || 'BROWSERSTACK_ACCESS_KEY',
+  user: process.env.BROWSERSTACK_USERNAME || 'username',
+  key: process.env.BROWSERSTACK_ACCESS_KEY || 'accesskey',
+
+  hostname: 'hub.browserstack.com',
 
   services: [
     [
       'browserstack',
-      { 
+      {
+        accessibility: false,
+        browserstackLocal: true,
         buildIdentifier: '${BUILD_NUMBER}',
-        browserstackLocal: true, 
-        opts: { forcelocal: false, localIdentifier: "webdriverio-appium-app-browserstack-android-repo" },
-        app: process.env.BROWSERSTACK_APP_PATH || './examples/LocalSample.apk'
+        app: process.env.BROWSERSTACK_APP_PATH || './examples/WikipediaSample.apk',
       }
     ]
   ],
 
   capabilities: [{
     'bstack:options': {
-      projectName: "BrowserStack Samples",
-      buildName: 'browserstack build',
-      sessionName: 'BStack local webdriverio-appium',
-      deviceName: 'Google Pixel 3',
-      osVersion: "9.0",
-      debug: true,
-      networkLogs: true,
-      source: 'webdriverio:appium-sample-sdk:v1.0'
+      deviceName: 'Google Pixel 8',
+      osVersion: "14.0"
+    }
+  }, {
+    'bstack:options': {
+      deviceName: 'Samsung Galaxy S21',
+      osVersion: "11.0"
     }
   }],
 
+  commonCapabilities: {
+    'bstack:options': {
+      projectName: "WDIO A11y",
+      buildName: 'testing',
+      sessionName: 'BStack parallel webdriverio-appium',
+      debug: true,
+      networkLogs: false,
+      source: 'webdriverio:appium-sample-sdk:v1.0'
+    }
+  },
+
+  maxInstances: 10,
+
   updateJob: false,
   specs: [
-    './examples/run-local-test/specs/local_test.js'
+    './specs/local_test.js'
   ],
   exclude: [],
 
@@ -44,6 +58,12 @@ exports.config = {
   framework: 'mocha',
   mochaOpts: {
     ui: 'bdd',
-    timeout: 20000
+    timeout: 40000
   }
 };
+
+// Code to support common capabilities
+exports.config.capabilities.forEach(function(caps){
+  for(let key in exports.config.commonCapabilities) 
+    caps[key] = { ...caps[key], ...exports.config.commonCapabilities[key]};
+});
